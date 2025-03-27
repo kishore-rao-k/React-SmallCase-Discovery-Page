89628c509c91e1f55081fe1ac4c0c991f6416dea
kishore-rao-k <kishoreraok12@gmail.com>
2025-03-27 10:29:12 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import data from "./data/smallcases.json"
+
+function App() {
+  return (
+    <>
+    <h1 className="text-amber-900">{JSON.stringify(data)}</h1>
+    </>
+  )
+}
+
+export default App
